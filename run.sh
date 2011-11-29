2d78d7139a1835373b6c734f90057d787157ea66
Paolo Castagna <paolo.castagna@gmail.com>
2011-11-29 00:33:40 +0000

    All files are now validated... added a script to install download and install Apache Jena.

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ ! -f "./apache-jena-2.6.5-incubating-20111123.112838-1-distribution.zip" ]; then
+    echo "==== Downloading Apache Jena v2.6.5-incubating-SNAPSHOT distribution ..."
+    wget https://repository.apache.org/content/repositories/snapshots/org/apache/jena/apache-jena/2.6.5-incubating-SNAPSHOT/apache-jena-2.6.5-incubating-20111123.112838-1-distribution.zip
+else
+    echo "==== [skipped] Downloading Apache Jena v2.6.5-incubating-SNAPSHOT distribution ..."
+
+fi
+
+if [ ! -d "./apache-jena" ]; then
+    echo "==== Unzipping Apache Jena v2.6.5-incubating-SNAPSHOT ..."
+    unzip apache-jena-2.6.5-incubating-20111123.112838-1-distribution.zip
+    mv apache-jena-2.6.5-incubating-SNAPSHOT apache-jena
+else
+    echo "==== [skipped] Unzipping Apache Jena v2.6.5-incubating-SNAPSHOT ..."
+fi
+
+PWD=`pwd`
+export JENAROOT="$PWD/apache-jena"
+export ARQROOT="$PWD/apache-jena"
+export TDBROOT="$PWD/apache-jena"
+
+
+echo "Validating cheese.ttl file ..." 
+$JENAROOT/bin/riot --validate cheese.ttl
+for file in *.trig; do 
+    echo "Validating $file file ..." 
+    $JENAROOT/bin/riot --validate "${file}"
+done
+
+echo "Loading cheese.ttl file ..." 
+$JENAROOT/bin/tdbloader --loc /tmp/cheese *.trig
+$JENAROOT/bin/tdbloader --loc /tmp/cheese cheese.ttl
+
+$JENAROOT/bin/tdbdump --loc /tmp/cheese > cheese.nq
+
+
