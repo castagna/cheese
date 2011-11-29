3404ba593b1d0b180d262900409a161791f5a09a
Paolo Castagna <paolo.castagna@gmail.com>
2011-11-29 12:14:45 +0000

    Added a schelethon for a Cheese Exhibit (using Exhibit2).

---
+++ exhibit/cheese.js
@@ -0,0 +1,30 @@
+{
+    "items": [
+        {   
+            "label":          "Asiago",
+            "type":           "Cheese",
+            "number":         "1",
+            "milk":           "cow (100%)",
+            "origin":         "Treviso, Italy",
+            "origin":         "Trento, Italy",
+            "latLng":         "45.875, 11.508",
+            "link":           "http://wikipedia.org/"
+        },
+        {   
+            "label":          "Bitto",
+            "type":           "Cheese",
+            "number":         "1",
+            "milk":           "cow (90%), goat (10%)",
+            "origin":         "Sondrio, Italy",
+            "latLng":         "46.060, 9.557",
+            "link":           "http://wikipedia.org/"
+        }
+    ],
+    types: {
+        'Cheese': {
+            pluralLabel: 'Cheeses'
+        }
+    },
+    properties: {
+    }
+}
